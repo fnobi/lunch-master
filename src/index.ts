cf55d69cd78e693b79d8c299ee9d887050244230
nobii <fujisawa-shin@kayac.com>
2020-03-16 23:13:05 +0900

    シートからconfigをひっぱってくるしくみ

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+type SheetName = "config";
+
 function main() {
-    console.log('hello, world!');
+    const config = importConfigFromSheet('config');
+    Logger.log(config);
+}
+
+function importConfigFromSheet(sheetName: SheetName) {
+    const config: {[key: string]: string} = {};
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = spreadsheet.getSheetByName(sheetName);
+    if (sheet) {
+        const range = sheet.getRange("A:B").getValues();
+        range
+            .filter(([key, value]) => key && value)
+            .forEach(([key, value]) => {
+                config[key.toString()] =value.toString();
+            });
+    }
+    return config;
 }
